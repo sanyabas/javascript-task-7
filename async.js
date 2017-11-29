a1cc4b4416be1dfcada6ed63fac55473e17e290a
Александр Белёв <belyov97@gmail.com>
2017-11-29 15:10:28 +0500

    first attempt

--- async.js
+++ async.js
@@ -3,11 +3,44 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
+
 /** Функция паралелльно запускает указанное число промисов
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            resolve([]);
+        }
+        const jobsCount = jobs.length;
+        let jobIndex = 0;
+        const results = [];
+        for (jobIndex; jobIndex < parallelNum; jobIndex++) {
+            start(jobs.shift(), jobIndex);
+        }
+
+        function processResult(result, counter) {
+            results[counter] = result;
+            if (Object.keys(results).length === jobsCount) {
+                resolve(results);
+
+                return;
+            }
+            if (jobs.length) {
+                start(jobs.shift(), jobIndex);
+                jobIndex++;
+            }
+        }
+
+        function start(job, counter) {
+            let process = result => processResult(result, counter);
+            new Promise((resolveJob, rejectJob) => {
+                job().then(resolveJob, rejectJob);
+                setTimeout(rejectJob, timeout, new Error('Promise timeout'));
+            }).then(process, process);
+        }
+    });
 }
